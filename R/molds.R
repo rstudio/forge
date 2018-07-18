d02dd1b7078b3461f55aab726b4fbfe8cb4954fc
kevinykuo <kevinykuo@gmail.com>
2018-07-18 01:58:18 -0400

    add backticks in error messages

--- R/molds.R
+++ R/molds.R
@@ -1,16 +1,16 @@
 #' @export
 mold_integer <- function(x, n = NULL, allow_na = FALSE, allow_null = FALSE) {
   if (is.null(x)) {
-    if (allow_null) return(x) else stop("x must not be NULL.", call. = FALSE)
+    if (allow_null) return(x) else stop("`x` must not be NULL.", call. = FALSE)
   }
 
   if (any(is.na(x))) {
-    if (!allow_na) stop("x must not contain NA.", call. = FALSE)
+    if (!allow_na) stop("`x` must not contain NAs.", call. = FALSE)
   }
 
   if (!is.null(n)) {
     length_x <- length(x)
-    if (length_x != n) stop("x must be of length ", n, ", but is of length ", length_x, ".",
+    if (length_x != n) stop("`x` must be of length ", n, ", but is of length ", length_x, ".",
                             call. = FALSE)
   }
 
--- tests/testthat/test-molds.R
+++ tests/testthat/test-molds.R
@@ -3,14 +3,14 @@ context("molds")
 test_that("mold_integer() works properly", {
   expect_identical(mold_integer(c(1, 2)), 1:2)
   expect_identical(mold_integer(42), 42L)
-  expect_error(mold_integer(NULL), "x must not be NULL\\.")
+  expect_error(mold_integer(NULL), "`x` must not be NULL\\.")
   expect_identical(mold_integer(NULL, allow_null = TRUE), NULL)
-  expect_error(mold_integer(1:3, 2), "x must be of length 2, but is of length 3\\.")
+  expect_error(mold_integer(1:3, 2), "`x` must be of length 2, but is of length 3\\.")
   expect_error(mold_integer(1.2), "Can't convert a fractional double vector to an integer vector")
-  expect_error(mold_integer(NA), "x must not contain NA\\.")
+  expect_error(mold_integer(NA), "`x` must not contain NAs\\.")
   expect_identical(mold_integer(c(2, NA), allow_na = TRUE), c(2L, NA))
 })
 
 test_that("mold_scalar_integer() works properly", {
-  expect_error(mold_scalar_integer(1:3), "x must be of length 1, but is of length 3.")
+  expect_error(mold_scalar_integer(1:3), "`x` must be of length 1, but is of length 3.")
 })
