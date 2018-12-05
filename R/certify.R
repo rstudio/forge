b32f7989de65505f86df5dcb9875c29f439c7268
kevinykuo <kevinykuo@gmail.com>
2018-12-04 21:57:14 -0500

    x -> .x

--- R/certify.R
+++ R/certify.R
@@ -2,18 +2,25 @@
 #'
 #' Checks that the input value satisfies specified conditions
 #'
-#' @param x The value to be checked.
+#' @param .x The value to be checked.
 #' @param ... Conditions to be checked; should be functions that return TRUE/FALSE.
 #' @param .l Lower bound for the inequality condition.
 #' @param .u Upper bound for the inequality condition.
 #' @export
-certify <- function(x, ...) {
+certify <- function(.x, ...) {
   quos <- rlang::enquos(...)
 
   for (quo in quos) {
     condition <- rlang::as_function(rlang::eval_tidy(quo))
+    satisfied <- condition(.x)
 
-    if (!condition(x)) stop(
+    if (!rlang::is_scalar_logical(satisfied)) stop(
+      "`",
+      rlang::quo_text(quo),
+      "` does not evaluate to a scalar logical for the given input."
+    )
+
+    if (!satisfied) stop(
       "Condition `",
       rlang::quo_text(quo),
       "` not satisfied.",
@@ -21,35 +28,39 @@ certify <- function(x, ...) {
     )
   }
 
-  x
+  .x
 }
 
 #' @rdname certify
 #' @export
 gt <- function(.l) {
   force(.l)
-  function(.x) .x > .l
+  .l <- cast_scalar_double(.l)
+  function(..x) cast_scalar_double(..x) > .l
 }
 
 #' @rdname certify
 #' @export
 gte <- function(.l) {
   force(.l)
-  function(.x) .x >= .l
+  .l <- cast_scalar_double(.l)
+  function(..x) cast_scalar_double(..x) >= .l
 }
 
 #' @rdname certify
 #' @export
 lt <- function(.u) {
   force(.u)
-  function(.x) .x < .u
+  .u <- cast_scalar_double(.u)
+  function(..x) cast_scalar_double(..x) < .u
 }
 
 #' @rdname certify
 #' @export
 lte <- function(.u) {
   force(.u)
-  function(.x) .x <= .u
+  .u <- cast_scalar_double(.u)
+  function(..x) cast_scalar_double(..x) <= .u
 }
 
 #' @rdname certify
@@ -59,9 +70,9 @@ between <- function(.l, .u, strict = c("neither", "lower", "upper", "both")) {
   strict <- match.arg(strict)
   switch(
     strict,
-    neither = function(.x) gte(.l)(.x) && lte(.u)(.x),
-    lower = function(.x) gt(.l)(.x) && lte(.u)(.x),
-    upper = function(.x) gte(.l)(.x) && lt(.u)(.x),
-    both = function(.x) gt(.l)(.x) && lt(.u)(.x)
+    neither = function(..x) gte(.l)(..x) && lte(.u)(..x),
+    lower = function(..x) gt(.l)(..x) && lte(.u)(..x),
+    upper = function(..x) gte(.l)(..x) && lt(.u)(..x),
+    both = function(..x) gt(.l)(..x) && lt(.u)(..x)
   )
 }
