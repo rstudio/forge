97fc5ce3a600566989ba8f1bbd844ad30f790f10
kevinykuo <kevinykuo@gmail.com>
2019-02-13 02:31:12 -0500

    Revert "Merge pull request #6 from kevinykuo/feature/reintroduce-stamp"
    This reverts commit 718a2655e4eec640e1efa0fa67220deb4215b52a, reversing
    changes made to aab9a5bb81f3070a2793e14abbc1d2794b46e8c3.

--- R/cast_utils.R
+++ R/cast_utils.R
@@ -26,31 +26,12 @@ verify_length <- function(x, n, id) {
 }
 
 as_nullable_list <- function(x) {
-  id <- resolve_id(x, NULL)
-  if (is.null(x)) return(x) else new_forge_stamped(rlang::as_list(x), id = id)
+  if (is.null(x)) return(x) else rlang::as_list(x)
 }
 
 backticks <- function(.s) paste0("`", .s, "`")
 
-new_forge_stamped <- function(x, id) {
-  structure(x, id = id, class = c("forge_stamped", class(x)))
-}
-
 get_id <- function(x) attr(x, "id", exact = TRUE)
-
-#' @export
-Ops.forge_stamped <- function(e1, e2) `attributes<-`(NextMethod(), list())
-
-#' @export
-Math.forge_stamped <- function(x, ...) `attributes<-`(NextMethod(), list())
-
-#' @export
-Complex.forge_stamped <- function(z) `attributes<-`(NextMethod(), list())
-
-#' @export
-print.forge_stamped <- function(x, ...) {
-  print(c(x))
-}
 
 resolve_id <- function(x, id) {
   expr <- rlang::quo_squash(x)
@@ -58,8 +39,6 @@ resolve_id <- function(x, id) {
 
   # provided id has precedence
   id %||%
-    # if x is the output of another forge function, get the id
-    (if (inherits(x, "forge_stamped")) get_id(x)) %||%
     # grab the user input for x
     (if (rlang::is_symbol(expr)) {
       id_string <- rlang::expr_text(expr)
